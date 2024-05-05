a7ac137b636cacb7cfb0a3df1957c68294bb8de4
Hari Krishna Bista <harikrista@gmail.com>
2024-05-04 18:52:30 -0700

    Update swiftly.swift

--- swiftly.swift
+++ swiftly.swift
@@ -318,3 +318,613 @@ let res1 = sortPlayers([a,b,c,d])
 for item in res1 {
     print(item)
 }
+
+
+func fibonaci(limit: Int) -> [Int] {
+    
+    var fibSeries: [Int] = []
+    
+    var first = 0
+    var second = 1
+    
+    var tempSum = first + second
+    
+    fibSeries.append(first)
+    fibSeries.append(second)
+    
+    while first + second < limit {
+        tempSum = first + second
+        
+        fibSeries.append(tempSum)
+        
+        first = second
+        second = tempSum
+    }
+    
+    return fibSeries
+}
+
+/*
+ let fibSeries = fibonaci(limit: 30)
+ 
+ for item in fibSeries {
+ print(item)
+ }
+ */
+
+// abcdef -> fedcba
+
+// Hari -> irah
+// exchange 0 and (count-1)
+// exchange 1 and (count-2)
+
+// ...
+// ...
+// exchange i'th and (count-i)'th item
+
+// loop runs 0 to (count/2-1)
+
+// Harik
+//
+
+func stringResersal1(str: String) -> String {
+    
+    var charArr = Array(str)
+    
+    var result = charArr
+    result.removeAll()
+    
+    while charArr.isEmpty == false {
+        result.append(charArr.removeLast())
+    }
+    
+    return String(result)
+}
+
+func stringResersal2(str: String) -> String {
+    
+    var charArr = Array(str)
+    
+    var i = 0
+    
+    while i < charArr.count/2 {
+        
+        let temp = charArr[i]
+        charArr[i] = charArr[charArr.count-1-i]
+        charArr[charArr.count-1-i] = temp
+        
+        i = i + 1
+    }
+    
+    return String(charArr)
+}
+
+
+/*
+ print(stringResersal1(str: "abcdef"))
+ print(stringResersal2(str: "abcdef"))
+ */
+
+
+// Integer to Roman on leetcode question
+
+/*
+ Symbol    Value
+ I         1
+ V         5
+ X         10
+ L         50
+ C         100
+ D         500
+ M         1000
+ */
+
+/*
+ 
+ 3 = iii
+ 4 = iv
+ 5 = v
+ 6 = vi
+ 7 = viii
+ 
+ 8 = viii
+ 9 = ix
+ 
+ */
+
+class Solution {
+    func intToRoman(_ num: Int) -> String {
+        return ""
+    }
+}
+
+/*
+ Input: nums = [8,7,11,15,1], target = 9
+ Output: [0,1]
+ Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+ */
+
+/*
+ 
+ dict
+ [
+ 1:0,
+ 2:1,
+ -2:2,
+ -6:3
+ ]
+ 
+ 
+ */
+
+
+class Solution_twoSum {
+    
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        
+        var dict: [Int : Int] = [:]
+        
+        for (i,num) in nums.enumerated() {
+            
+            let diff = target - num
+            
+            if let index = dict[num] {
+                return [index, i]
+            }
+            
+            dict[diff] = i
+        }
+        
+        return []
+    }
+    
+}
+
+//print(Solution_twoSum().twoSum([8,1,1,15,1,7], 9))
+
+// https://leetcode.com/problems/rotate-array/description/
+// Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+/*
+ 
+ Input: nums = [1,2,3,4,5,6,7], k = 3
+ Output: [5,6,7,1,2,3,4]
+ Explanation:
+ rotate 1 steps to the right: [7,1,2,3,4,5,6]
+ rotate 2 steps to the right: [6,7,1,2,3,4,5]
+ rotate 3 steps to the right: [5,6,7,1,2,3,4]
+ 
+ */
+
+/*
+ 
+ temp = 6
+ [7,1,2,3,4,5,6]
+ 
+ [7,7,1,2,3,4,5]
+ [7,7,1,2,3,4,5]
+ 
+ */
+
+
+
+
+
+class Solution_rotate1 {
+    
+    func rotate(_ nums: inout [Int], _ k: Int) {
+        for i in 0...(k-1) {
+            shift(arr: &nums)
+        }
+    }
+    
+    private func shift(arr: inout [Int]) {
+        
+        guard arr.count > 0 else { return }
+        
+        let temp = arr[arr.count-1]
+        
+        var index = arr.count-1
+        
+        while index > 0 {
+            arr[index] = arr[index-1]
+            index = index - 1
+        }
+        
+        arr[0] = temp
+    }
+}
+
+// [1,2,3,4,5,6,7]
+//  0 1 2 ..
+
+// "Hari", "Krishna", "Bista"
+
+class Solution_rotate2 {
+    func rotate(_ nums: inout [Int], _ k: Int) {
+        
+        var newArr = [Int](repeating: 0, count: nums.count)
+        
+        for (i, num) in nums.enumerated() {
+            
+            var newIndex = i + k
+            if newIndex > (nums.count-1) {
+                newIndex = newIndex - nums.count
+            }
+            
+            newArr[newIndex] = num
+        }
+        
+        nums = newArr
+    }
+}
+
+/*
+ var input_nums1 = [1,2,3,4,5,6,7]
+ Solution_rotate1().rotate(&input_nums, 3)
+ print(input_nums)
+ */
+
+// [1,2,3,4,5,6,7], 3
+// [_,_,_,1,2,3,4], 5,6,7
+
+/*
+ var input_nums1 = [1,2,3,4,5,6,7]
+ Solution_rotate2().rotate(&input_nums1, 3)
+ print(input_nums1)
+ */
+
+class Solution_powerSet {
+    // non recursive solution
+    func subsets(_ nums: [Int]) -> [[Int]] {
+        var result:[[Int]] = []
+        result.append([])
+        
+        for item in nums {
+            product(item: item, result: &result)
+        }
+        
+        return result
+    }
+    
+    private func product(item:Int, result: inout [[Int]]) {
+        
+        print(item, result)
+        
+        for items in result {
+            var newItems = items
+            newItems.append(item)
+            result.append(newItems)
+        }
+    }
+}
+
+// let res = Solution_powerSet().subsets([1,2,3])
+
+
+/*
+ Write a program that prints the numbers from 1 to n. But for multiples of three, print "Fizz" instead of the number,
+ and for the multiples of five, print "Buzz". For numbers that are multiples of both three and five, print "FizzBuzz".
+ */
+
+func fizzBuzz(n: Int) {
+    
+    for number in 1...n {
+        
+        if number % (3*5) == 0 {
+            print("FizzBuzz")
+        } else if number % 3 == 0 {
+            print("Fizz")
+        } else if number % 5 == 0 {
+            print("Buzz")
+        }
+    }
+    
+}
+
+//fizzBuzz(n: 100)
+
+
+// "listen" and "silent" are anagrams
+func areAnagrams(_ str1: String, _ str2: String) -> Bool {
+    str1.sorted() == str2.sorted()
+}
+
+
+class Solution_findAnagrams {
+    func findAnagrams(_ s: String, _ p: String) -> [Int] {
+        
+        guard s.count >= p.count else { return [] }
+        
+        let strings = getAllSubStrings(str: s, subStringSize: p.count)
+        
+        let key = p.sorted()
+        
+        var result: [Int] = []
+        
+        for (i,str) in strings.enumerated() {
+            if str.sorted() == key {
+                result.append(i)
+            }
+        }
+        
+        return result
+    }
+    
+    private func getAllSubStrings(str: String, subStringSize: Int) -> [String] {
+        var result: [String] = []
+        
+        for i in 0...(str.count - subStringSize) {
+            let subStr = getSubString(fromStr: str, startIndex: i, length: subStringSize)
+            result.append(subStr)
+        }
+        
+        return result
+    }
+    
+    private func getSubString(fromStr: String, startIndex: Int, length: Int) -> String {
+        
+        var result: [Character] = []
+        
+        let charArr = Array(fromStr)
+        
+        for i in startIndex...(startIndex + length - 1) {
+            result.append(charArr[i])
+        }
+        
+        return String(result)
+    }
+}
+
+//print(Solution_findAnagrams().findAnagrams("cbaebabacd", "abc"))
+//print(Solution_findAnagrams().findAnagrams("abab", "ab"))
+
+//print(getSubString(fromStr: "Harik", startIndex: 0, length: 3) == "Har")
+//print(getSubString(fromStr: "Harik", startIndex: 1, length: 4) == "arik")
+
+func solution(
+    _ start: [Int],
+    _ dest: [Int],
+    _ limit: [Int]
+) -> Int {
+    var maxStation = 0
+    var totalFee = 0
+    
+    for i in 0..<start.count {
+        let startStation = min(start[i], dest[i])
+        let endStation = max(start[i], dest[i])
+        
+        totalFee = totalFee + (endStation - startStation) * 2
+        
+        maxStation = max(maxStation, endStation)
+    }
+    
+    totalFee = totalFee + start.count * 1
+    
+    return min(totalFee, limit[maxStation])
+}
+
+/*
+ let start = [1, 0, 2, 4]
+ let dest = [2, 2, 0, 5]
+ 
+ let limit = [3, 17, 7, 4, 5, 17]
+ 
+ print(solution(start, dest, limit))
+ */
+
+
+/*
+ 
+ 
+ let T = [3,1,2]
+ 
+ doneCounter = 3
+ 
+ var workRecord = [2,0,1,1,0,0]
+ 
+ while doneCounter > 3 {
+ T = process(T)
+ }
+ 
+ func process(T) -> T {
+ 
+ }
+ 
+ let result = scan(workRecord)
+ 
+ h  h  h
+ T = [3, 1, 2]
+ 2  0  1 -> [2,1]
+ 1     0 -> [1]
+ 0       -> []
+ 
+ 2 0 1 1 0 0
+ 
+ */
+
+
+class SolutionT {
+    
+    func calculateTotalWaitHours(hours: [Int]) -> Int {
+        
+        var remHours: [Int] = hours
+        
+        var completedRecords: [Int] = []
+        
+        while !remHours.isEmpty {
+            
+            var nextHours: [Int] = []
+            
+            for hr in remHours {
+                let remHour = hr - 1
+                
+                if remHour > 0 {
+                    nextHours.append(remHour)
+                }
+                
+                completedRecords.append(remHour)
+            }
+            
+            remHours = nextHours
+        }
+        
+        return getTotalHours(record: completedRecords)
+    }
+    
+    // [2,0,1,1,0,0]
+    func getTotalHours(record: [Int]) -> Int {
+        var totalHours = 0
+        for (i,number) in record.enumerated() {
+            if number == 0 {
+                totalHours += (i + 1)
+            }
+        }
+        return totalHours
+    }
+}
+
+/*
+ let res = SolutionT().calculateTotalWaitHours(hours: [3,1,2])
+ print(res)
+ */
+
+class Solution_isValidSudoku {
+    
+    func isValidSudoku(_ board: [[Character]]) -> Bool {
+        isRowsValid(board: board) && isColumnsValid(board: board)
+    }
+    
+    func isRowsValid(board: [[Character]]) -> Bool {
+        for row in board {
+            if isItemsUnique(items: row) == false {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func isColumnsValid(board: [[Character]]) -> Bool {
+        var col = 0
+        let size = board.count
+        
+        while col < size {
+            
+            var tempItems: [Character] = []
+            
+            for row in 0..<size {
+                let item = board[row][col]
+                tempItems.append(item)
+            }
+            
+            if isItemsUnique(items: tempItems) == false {
+                return false
+            }
+            
+            col = col + 1
+            
+        }
+        
+        return true
+    }
+    
+    
+    
+    func isItemsUnique (items: [Character]) -> Bool {
+        
+        var seen = Set<Character>()
+        
+        for item in items {
+            if seen.contains(item) {
+                return false
+            }
+            
+            if item != "." {
+                seen.insert(item)
+            }
+        }
+        
+        return true
+    }
+}
+
+
+//print(Solution_isValidSudoku().isValidSudoku(sodukuGrid))
+
+import Darwin
+
+func isValidSudoku(_ board: [[Character]]) -> Bool {
+    
+    let boardSize = board.count
+    
+    var rows = Array(repeating: Set<Character>(), count: boardSize)
+    var columns = Array(repeating: Set<Character>(), count: boardSize)
+    
+    var boxes = Array(repeating: Set<Character>(), count: boardSize)
+    var boxCounter = 0
+    
+    let miniBoardSize = Int(sqrt(Double(boardSize)))
+    
+    for i in 0..<boardSize {
+        for j in 0..<boardSize {
+            let char = board[i][j]
+            
+            if char != "." {
+                if rows[i].contains(char) || columns[j].contains(char) || boxes[j / miniBoardSize].contains(char) {
+                    return false
+                }
+                
+                rows[i].insert(char)
+                columns[j].insert(char)
+                
+                boxes[j / miniBoardSize].insert(char)
+            }
+            
+            boxCounter = boxCounter + 1
+            
+            if boxCounter == boardSize * miniBoardSize {
+                boxCounter = 0
+                boxes = Array(repeating: Set<Character>(), count: 3)
+            }
+        }
+    }
+    
+    return true
+}
+
+let sodukuGrid:[[Character]] =
+[
+    ["5","3",".",".","7",".",".",".","."],
+    ["6",".",".","1","9","5",".",".","."],
+    [".","9","8",".",".",".",".","6","."],
+    ["8",".",".",".","6",".",".",".","3"],
+    ["4",".",".","8",".","3",".",".","1"],
+    ["7",".",".",".","2",".",".",".","6"],
+    [".","6",".",".",".",".","2","8","."],
+    [".",".",".","4","1","9",".",".","5"],
+    [".",".",".",".","8",".",".","7","9"]
+]
+
+func printCharacterMatrix(_ matrix: [[Character]]) {
+    
+    for i in 0..<matrix.count {
+        
+        for j in 0..<matrix[i].count {
+            if i % 3 == 0 && j % 3 == 0 {
+                print("🟢 [\(i),\(j)] - \(matrix[i][j])", terminator: "  ")
+            } else {
+                print("💜 (\(i),\(j)) - \(matrix[i][j])", terminator: "  ")
+            }
+        }
+        print("")
+        print("")
+    }
+    
+}
+
+printCharacterMatrix(sodukuGrid)
+
+print(isValidSudoku(sodukuGrid))
+
+print("")
+
